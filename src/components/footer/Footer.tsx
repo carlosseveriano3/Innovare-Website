8b053e4fdbcfe402d1a170495c9d5ca641bcb997
carlosseveriano3 <carlosseveriano3@gmail.com>
2024-11-17 01:04:29 -0300

    finishing main page's footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="content">
         <div>
           <h1>INSTITUCIONAL</h1>
-          <ul>
+          <ul className="line-bar-institucional">
             <li>Principal</li>
             <li>Sobre</li>
             <li>Consultoria, Inspeção e Projetos</li>
@@ -15,6 +15,17 @@ const Footer = () => {
             <li>Treinamentos</li>
             <li>Contato</li>
           </ul>
+        </div>
+
+        <div>
+          <h1>CONTATO</h1>
+          <div className="contact line-bar-contato">
+            <span>Endereço: R. Tibúrcio Cavalcante, 1958 - B</span>
+            <span>Aldeota - Fortaleza - CE</span>
+            <span>Telefone: (85) 98788.7078</span>
+            <span>E-mail: atendimento@hesq.com.br</span>
+            <span>CNPJ: 54.503.236/0001-70</span>
+          </div>
         </div>
       </div>
     </section>
